9bd1120659d1c6e3bb3697a59393f2036ece7946
Edoardo Mucelli <edoardomucelli@gmail.com>
2026-02-01 18:18:23 +0000

    Now make the text "Alice" underlined and make it a hyperlink linking to

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,6 +46,10 @@ export default function Home() {
           <p>At heart, I consider myself a builder. I like to figure out how things work and make the best out of them.</p>
         </div>
 
+        <div className="text-block">
+          <p>Currently I am the Product Designer at <a href="https://www.alice.tech/" target="_blank" rel="noopener noreferrer"><u>Alice</u></a> (previously Alice.tech) shaping a more intelligent way for students to learn. 
+I mix high-level strategy with the CEO and hands-on prototyping. My job is owning the big picture to ensure we’re always innovating and shipping the best UX.</p>
+        </div>
 
 
         <div className="media-hug-content w-full">
@@ -57,11 +61,6 @@ export default function Home() {
             className="h-auto mx-auto"
             data-ai-hint="logo placeholder"
           />
-        </div>
-
-        <div className="text-block">
-          <p>Currently I am the Product Designer at Alice (previously Alice.tech) shaping a more intelligent way for students to learn. 
-I mix high-level strategy with the CEO and hands-on prototyping. My job is owning the big picture to ensure we’re always innovating and shipping the best UX.</p>
         </div>
 
         <div className="media-hug-content w-full">
